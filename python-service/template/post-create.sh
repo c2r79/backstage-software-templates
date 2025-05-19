988bffcffb35797591b6fc51c2a2776a57f1328e
c2r79 <pharmsale7@gmail.com>
2025-05-19 13:23:40 -0400

    one more time

--- python-service/template/post-create.sh
+++ python-service/template/post-create.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
-RELEASE_REPO="https://github.com/los-demos/dcipher-demo-releases.git"
+REPO_NAME="dcipher-demo-releases"
+RELEASE_REPO="https://github.com/los-demos/$REPO_NAME.git"
 APP_NAME="${{ values.app_name }}"
 ENV="${{ values.app_env }}"
 
+git clone $RELEASE_REPO
+
+cd "$REPO_NAME"
 git config user.email "github-actions@los-demos.com"
 git config user.name "GitHub Actions Bot"
+cd ..
 
-git clone $RELEASE_REPO
-cp app-template.yaml dcipher-demo-releases/apps/$APP_NAME/$ENV-$APP_NAME.yaml
-sed -i "s/__APP_NAME__/${APP_NAME}/g" dcipher-demo-releases/apps/$APP_NAME/$ENV-$APP_NAME.yaml
-git -C dcipher-demo-releases add apps/$APP_NAME/$ENV-$APP_NAME.yaml
-git -C dcipher-demo-releases commit -m "Add ${APP_NAME} to ${ENV}"
-git -C dcipher-demo-releases push
+mkdir -p "$REPO_NAME/apps/${APP_NAME}"
+cp app-template.yaml $REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml
+sed -i "s/__APP_NAME__/${APP_NAME}/g" $REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml
+git -C $REPO_NAME add apps/$APP_NAME/$ENV-$APP_NAME.yaml
+git -C $REPO_NAME commit -m "Add ArgoCD release for ${APP_NAME}:${ENV}:${COMMIT_ID}"
+git -C $REPO_NAME push
