c762295fadcaa7df30c1dcbb2d50624aa1940bd1
c2r79 <pharmsale7@gmail.com>
2025-05-20 11:58:52 -0400

    cleaned up a lot of code

--- python-service/template/post-create.sh
+++ python-service/template/post-create.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
 
-APP_NAME="${{ values.app_name }}"
-ENV="${{ values.app_env }}"
+# Read values passed by GitHub Actions as environment variables
+APP_NAME="${APP_NAME}"
+ENV="${APP_ENV}"
+REPO_NAME="dcipher-demo-releases"
 
-git clone $RELEASE_REPO
+# Clone the target release repo
+git clone "$RELEASE_REPO"
 
+# Set Git identity for the commit
 cd "$REPO_NAME"
 git config user.email "github-actions@los-demos.com"
 git config user.name "GitHub Actions Bot"
 cd ..
 
+# Create target app directory if it doesn't exist
 mkdir -p "$REPO_NAME/apps/${APP_NAME}"
-cp app-template.yaml $REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml
-sed -i "s/__APP_NAME__/${APP_NAME}/g" $REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml
-git -C $REPO_NAME add apps/$APP_NAME/$ENV-$APP_NAME.yaml
-git -C $REPO_NAME commit -m "Add ArgoCD release for ${APP_NAME}:${ENV}:${COMMIT_ID}"
-git -C $REPO_NAME push
+
+# Copy and customize the ArgoCD app manifest
+cp app-template.yaml "$REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml"
+sed -i "s/__APP_NAME__/${APP_NAME}/g" "$REPO_NAME/apps/$APP_NAME/$ENV-$APP_NAME.yaml"
+
+# Commit and push to release repo
+git -C "$REPO_NAME" add "apps/$APP_NAME/$ENV-$APP_NAME.yaml"
+git -C "$REPO_NAME" commit -m "Add ArgoCD release for ${APP_NAME}:${ENV}:${COMMIT_ID}"
+git -C "$REPO_NAME" push
